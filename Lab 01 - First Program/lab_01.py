4771d9dac1dfe27de371be6507db07574171ee03
Trevor Egbert <trevor.egbert@gmail.com>
2022-01-19 15:45:54 -0700

    Lab 1 done

--- Lab 01 - First Program/lab_01.py
+++ Lab 01 - First Program/lab_01.py
@@ -1,3 +1,6 @@
-print("Why hello there world fine day we are having here today is it not?")
-print("It was a day like any other day when suddenly a...")
+print("This is my first time coding in Python.")
+print("I learned some C++ from a course on Udemy about coding in the Unreal Engine.")
+print("Python seems a little easier right now but...\nWe will have to see as time goes on.")
+print('I hope this will help me get better at coding!')
+
 
